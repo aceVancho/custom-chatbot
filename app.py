588ac50f7478e7f5cb0835cbc616b0416108da50
Ace <aevancho@me.com>
2023-04-05 12:54:59 -0500

    use beautiful soup to create index

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from gpt_index import SimpleDirectoryReader, GPTListIndex, GPTSimpleVectorIndex, LLMPredictor, PromptHelper, ServiceContext
-from langchain import OpenAI
-import gradio as gr
 import sys
 import os
+import requests
+
+import gradio as gr
+from gpt_index import SimpleDirectoryReader, GPTListIndex, GPTSimpleVectorIndex, LLMPredictor, PromptHelper, ServiceContext
+from llama_index import download_loader, GPTSimpleVectorIndex
+from langchain import OpenAI
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse, urljoin
 
 OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 
-def construct_index(directory_path):
+url = "https://gpt-index.readthedocs.io/en/latest/index.html"
+
+def construct_index_from_docs(directory_path):
     max_input_size = 4096
     num_outputs = 512
     max_chunk_overlap = 20
@@ -24,6 +31,28 @@ def construct_index(directory_path):
 
     index.save_to_disk('index.json')
 
+def construct_index_from_website(url):
+    max_input_size = 4096
+    num_outputs = 512
+    max_chunk_overlap = 20
+    chunk_size_limit = 600
+
+    prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
+
+    llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.7, model_name="gpt-4", max_tokens=num_outputs))
+
+    service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+
+    BeautifulSoupWebReader = download_loader("BeautifulSoupWebReader")
+
+    loader = BeautifulSoupWebReader()
+
+    documents = loader.load_data(urls=[url])
+
+    index = GPTSimpleVectorIndex.from_documents(documents)
+
+    index.save_to_disk('index.json')
+
 def chatbot(input_text):
     index = GPTSimpleVectorIndex.load_from_disk('index.json')
     response = index.query(input_text, response_mode="compact")
@@ -32,7 +61,9 @@ def chatbot(input_text):
 iface = gr.Interface(fn=chatbot,
                      inputs=gr.inputs.Textbox(lines=7, label="Enter your text"),
                      outputs="text",
-                     title="Custom-trained AI Chatbot")
+                     title="Informed Chatbot")
+# Pick one
+# index = construct_index_from_docs('docs')
+index = construct_index_from_website(url)
 
-index = construct_index("docs")
 iface.launch(share=True)
